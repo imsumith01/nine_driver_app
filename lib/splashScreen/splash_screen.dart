ae98ef901276201ed14cfd55345195e4f982cf43
Imtiaz Sumith <imtiaz@onedrop.today>
2023-05-06 21:12:46 -0400

    set the splash screen with logo and text

---
+++ lib/splashScreen/splash_screen.dart
@@ -0,0 +1,55 @@
+import 'dart:async';
+
+import 'package:driver_app/mainScreens/main_screen.dart';
+import 'package:flutter/material.dart';
+
+class MySplashScreen extends StatefulWidget {
+  const MySplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MySplashScreen> createState() => _MySplashScreenState();
+}
+
+class _MySplashScreenState extends State<MySplashScreen> {
+  startTimer() {
+    Timer(const Duration(seconds: 3), () async {
+      //send user to home screen
+      Navigator.push(
+          context, MaterialPageRoute(builder: (c) => const MainScreen()));
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    startTimer();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Container(
+        color: Colors.black,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset("images/logo-no-background.png"),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                "Driver App",
+                style: TextStyle(
+                    fontSize: 24,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
